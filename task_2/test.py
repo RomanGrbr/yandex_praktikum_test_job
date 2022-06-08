0f64111efd2c9526c727c4593b3d53e380fdbdb7
Роман <romangrbr@gmail.com>
2022-06-08 22:27:56 +0300

    probably finished working with the task_2

--- task_2/test.py
+++ task_2/test.py
@@ -2,18 +2,10 @@ import contextlib
 import importlib
 import io
 
+import pytest
+
 import author
 import precode
-
-import sys
-from os.path import abspath, dirname
-
-root_dir = dirname(dirname(abspath(__file__)))
-sys.path.append(root_dir)
-
-pytest_plugins = [
-    'task_2.precode',
-]
 
 # stdout работы файла с кодом студента (все его print()) в виде одной строки
 output = io.StringIO()
@@ -31,17 +23,61 @@ with contextlib.redirect_stdout(author_output):
     importlib.reload(author)
 
 
-def test_response_output():
-    assert output.getvalue() == author_output.getvalue(), (
-        "Erroe: Вы ошиблись в выводе на экран")
+class MsgError:
 
-    
-def test_method_show_contact():
-    assert hasattr(precode.Contact, "show_contact"), (
-        "Error: Проверьте наличие метода show_contact в классе Contact")
+    def __init__(self, method_name, *args, **kwargs):
+        self.result = getattr(self, method_name)(*args, **kwargs)
 
-def test_attribut():
-    author_attr = [value for value in author.__dict__ if not value.startswith('__')]
-    user_attr = [value for value in precode.__dict__ if not value.startswith('__')]
-    assert author_attr == user_attr, (
-        "Error: Проверьте нет ли лишних атрибутов")
+    def add_class(self, class_name, child=False, parent_name=""):
+        text = f"Добавьте класс `{class_name}`"
+        if child:
+            text += f", этот класс наследуется от класса `{parent_name}`"
+        return
+
+    def add_method(self, method_name, class_name):
+        return f"Добавьте метод `{method_name}()` для класса `{class_name}`"
+
+    def dont_create_def(self, def_name):
+        return f"`Функции `{def_name}()` не должно быть"
+
+
+@pytest.fixture
+def msg_err():
+    def _msg_err(msg_name, *args, **kwargs):
+        msg = MsgError(msg_name, *args, **kwargs)
+        return msg.result
+    return _msg_err
+
+
+class TestContact:
+
+    def test_method_show_contact(self, msg_err):
+        assert hasattr(precode.Contact, "show_contact"), msg_err(
+            "add_method", "show_contact", "Contact")
+        assert not hasattr(precode, "print_contact"), msg_err(
+            "dont_create_def", "print_contact")
+
+    def test_attr_name(self):
+        user = precode.Contact("Михаил Булгаков", "2-03-27", "15.05.1891", "Россия, Москва, Большая Пироговская, дом 35б, кв. 6")
+        author_data = author.vlad.__dict__
+        for key in author_data.keys():
+            assert hasattr(user, key), (
+                f"Error: Проверьте наличие атрибута {key} в классе Contact"
+            )
+
+    def test_extra_attributes(self):
+        author_attr = [
+            value for value in author.__dict__ if not value.startswith('__')]
+        user_attr = [
+            value for value in precode.__dict__ if not value.startswith('__')]
+
+        assert len(author_attr) >= len(user_attr), (
+            "Error: Убедитесь, что нет лишних переменных")
+        assert len(author_attr) <= len(user_attr), (
+            "Error: Убедитесь, что есть все переменные")
+        assert author_attr == user_attr, (
+            "Error: Убедитесь, что переменные названы верно")
+
+    def test_response_output(self):
+        assert output.getvalue() == author_output.getvalue(), (
+            "Error: Вы ошиблись в выводе на экран !")
