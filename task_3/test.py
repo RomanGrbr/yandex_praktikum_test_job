74c21819f0602acab5ec253e4d0badaa5686c623
Роман <romangrbr@gmail.com>
2022-06-10 21:52:48 +0300

    make tests for 3 abd 4 tasks

--- task_3/test.py
+++ task_3/test.py
@@ -1,7 +1,7 @@
 import contextlib
 import io
 import time
-import importlib
+
 import pytest
 
 # stdout работы файла с кодом студента (все его print()) в виде одной строки
@@ -22,6 +22,7 @@ with open("precode.py", encoding="utf-8") as task:
 with open("author.py", encoding="utf-8") as task:
     author_code = task.readlines()
 
+
 class MsgError:
 
     def __init__(self, method_name, *args, **kwargs):
@@ -29,6 +30,9 @@ class MsgError:
 
     def add_def(self, def_name):
         return f"Проверьте наличие функции `{def_name}()`"
+
+    def dont_cahge_def(self, def_name):
+        return f"Кажется вы изменили функцию `{def_name}()`"
 
 
 @pytest.fixture
@@ -38,6 +42,7 @@ def msg_err():
         return msg.result
     return _msg_err
 
+
 def time_check(func):
     def wrapper(*args):
         start_time = time.time()
@@ -46,49 +51,56 @@ def time_check(func):
         return execution_time
     return wrapper
 
+
 def foo(num):
     time.sleep(1)
     return num*2
 
-class TestTask:
 
-    def test_response_output(self):
-        assert output.getvalue() == author_output.getvalue(), (
-            "Error: Вы ошиблись в выводе на экран !")
+class TestTask:
 
     def test_have_def(self, msg_err):
         try:
             precode.time_check
         except AttributeError:
-            assert False, msg_err("add_def","time_check")
+            assert False, msg_err("add_def", "time_check")
         try:
             precode.cache_args
         except AttributeError:
-            assert False, msg_err("add_def","cache_args")
+            assert False, msg_err("add_def", "cache_args")
         try:
             precode.long_heavy
         except AttributeError:
-            assert False, msg_err("add_def","long_heavy")
+            assert False, msg_err("add_def", "long_heavy")
 
-    def test_time_check(self):
-        pass
-    
-    def test_long_heavy(self):
-        
+    # def test_time_check(self, msg_err):
+    #     us_tm_ch = io.StringIO()
+    #     with contextlib.redirect_stdout(us_tm_ch):
+    #         importlib.reload(precode)
+    #         precode.time_check(foo)(2)
 
-        assert precode.long_heavy(4) == author.long_heavy(4), (
-            "Не изменяйте функцию `long_heavy()`"
+    #     au_tm_ch = io.StringIO()
+    #     with contextlib.redirect_stdout(au_tm_ch):
+    #         importlib.reload(author)
+    #         author.time_check(foo)(2)
+
+    #     assert us_tm_ch.getvalue() == au_tm_ch.getvalue(), msg_err(
+    #         "dont_cahge_def", "time_check"
+    #     )
+
+    def test_long_heavy(self, msg_err):
+        assert precode.long_heavy(4) == author.long_heavy(4), msg_err(
+            "dont_cahge_def", "long_heavy"
         )
-        
-
 
     def test_cache_args(self):
-        # importlib.reload(precode)
-        # long_heavy = author.long_heavy.__closure__[0].cell_contents
-        cache_args = precode.cache_args
-        result = []
+        user_cache = precode.cache_args(foo)
+        author_cache = author.cache_args(foo)
         for i in (1, 1, 2, 2, 2):
-            result.append(time_check(cache_args(foo))(i))
-        assert result == [1.0, 0.0, 1.0, 0.0, 0.0], (
-            "Функция плохая"
-        )
+            assert time_check(user_cache)(i) == time_check(author_cache)(i), (
+                "Фунуция `cache_args()` работает не верно"
+            )
+
+    def test_response_output(self):
+        assert output.getvalue() == author_output.getvalue(), (
+            "Error: Вы ошиблись в выводе на экран !")
--- task_4/test.py
+++ task_4/test.py
@@ -1,1 +1,44 @@
+import contextlib
+import io
+import time
 
+# stdout работы файла с кодом студента (все его print()) в виде одной строки
+output = io.StringIO()
+with contextlib.redirect_stdout(output):
+    import precode
+
+# stdout работы файла с кодом автора (все его print()) в виде одной строки
+author_output = io.StringIO()
+with contextlib.redirect_stdout(author_output):
+    import author
+
+
+def time_check(func):
+    def wrapper(*args):
+        start_time = time.time()
+        func(*args)
+        execution_time = round(time.time() - start_time, 1)
+        return execution_time
+    return wrapper
+
+
+class TestTask:
+
+    def test_make_divider(self):
+        author_div2 = author.make_divider_of(2)
+        user_div2 = precode.make_divider_of(2)
+        assert author_div2(10) == user_div2(10), (
+            "Функция `make_divider()` работает не верно")
+
+    def test_time_make_divider(self):
+        author_div2 = author.make_divider_of(2)
+        user_div2 = precode.make_divider_of(2)
+        tm_au = time_check(author_div2)(10)
+        tm_us = time_check(user_div2)(10)
+        assert tm_au == tm_us, (
+            "Функция `make_divider()` выполняется слишком долго"
+        )
+
+    def test_response_output(self):
+            assert output.getvalue() == author_output.getvalue(), (
+                "Error: Вы ошиблись в выводе на экран !")
